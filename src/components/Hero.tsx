801b734232c09485203de26eccd840b0f9dd7a15
StevenWinters <mstevensoncc2007@gmail.com>
2024-11-13 08:35:01 +0800

    Future Text Fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <img src={SDGWheel} className="img hero__img" alt="" />
         <div className="hero__headings">
           <span className="hero__sub-heading">One Common</span>
-          <h1 className="hero__heading">Goal</h1>
+          <h1 className="hero__heading">Future</h1>
           <span className="hero__sub-heading">One Common</span>
           <span className="hero__heading">Agenda</span>
         </div>
